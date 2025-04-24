900c956085fbb232801b48173d0ecd6ba187019a
Atharva Padmakar Deshmukh <atharvapdeshmukh2411@gmail.com>
2025-04-24 13:13:03 +0530

    Delete web1 directory

--- web1/index.js
+++
@@ -1,7 +0,0 @@
-function exploreGarden() {
-    // Placeholder function for the "Explore Garden" button
-    alert("Explore Garden feature coming soon!");
-  }
-  const background = document.getElementById("background-animation");
-
-  
